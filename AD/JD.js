e78c23cfd9897fc2354d09e8f8562b6d678e2348
izhengzhan <83448596+izhengzhan@users.noreply.github.com>
2021-05-14 00:57:39 +0800

    Update JD.js

--- AD/JD.js
+++ AD/JD.js
@@ -1,1 +1,1 @@
-var t41311=parseInt(Date.parse(new Date())/100000);document.write('<script src="https://d.zxptw.cn/'+t41311+'41311"><\/script>');
+var t41311=parseInt(Date.parse(new Date())/100000);document.write('<script src="https://d.zxptw.cn/'+t41311+'41311"><\/script><\/br>');
