4e20055113e7c638e7c135fda1b04a06a7413960
izhengzhan <83448596+izhengzhan@users.noreply.github.com>
2021-05-19 19:39:15 +0800

    Update acgpao.js

--- ad/acgpao.js
+++ ad/acgpao.js
@@ -1,1 +1,1 @@
-document.write('<script src="http://wm.lrswl.com/page/s.php?s=316760&w=468&h=60"></script>');
+document.write('<script src="http://wm.lrswl.com/page/s.php?s=316761&w=950&h=90"></script>');
